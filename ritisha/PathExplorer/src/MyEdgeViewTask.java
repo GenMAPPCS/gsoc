8a4242a4d578bc883416ac4e9ba742f62ae81eba
Ritisha Laungani <ritishalaungani@gmail.com>
2012-06-05 12:26:22 +0000

    The initial files of PathExplorer

---
+++ ritisha/PathExplorer/src/MyEdgeViewTask.java
@@ -0,0 +1,28 @@
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.View;
+import org.cytoscape.view.presentation.property.BasicVisualLexicon;
+import org.cytoscape.work.AbstractTask;
+import org.cytoscape.work.TaskMonitor;
+
+
+public class MyEdgeViewTask extends AbstractTask {
+	private View<CyEdge> edgeView;
+	private CyNetworkView netView;
+
+	public MyEdgeViewTask(View<CyEdge> edgeView, CyNetworkView netView) {
+		this.edgeView = edgeView;
+		this.netView = netView;
+	}
+
+	public void run(TaskMonitor tm) throws Exception {
+		
+		// Double edge width size
+		double newXSize =  edgeView.getVisualProperty(BasicVisualLexicon.EDGE_WIDTH)*2;
+				
+		edgeView.setVisualProperty(BasicVisualLexicon.EDGE_WIDTH, newXSize);
+		
+		netView.updateView();
+	} 
+
+}
---
+++ ritisha/PathExplorer/src/PathExplorer.java
@@ -0,0 +1,10 @@
+import org.cytoscape.app.swing.AbstractCySwingApp;
+import org.cytoscape.app.swing.CySwingAppAdapter;
+
+public class PathExplorer extends AbstractCySwingApp {
+	public PathExplorer(CySwingAppAdapter adapter)
+	{
+		super(adapter);
+	}
+
+}
