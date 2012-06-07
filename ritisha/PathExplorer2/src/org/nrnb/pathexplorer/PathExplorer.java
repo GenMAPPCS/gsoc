128aa18be16b684ae49d50f30d46290fed200eef
Alexander Pico <apico@gladstone.ucsf.edu>
2012-06-07 05:08:53 +0000

    updates

--- ritisha/PathExplorer2/src/org/nrnb/pathexplorer/PathExplorer.java
+++ ritisha/PathExplorer2/src/org/nrnb/pathexplorer/PathExplorer.java
@@ -1,12 +1,21 @@
 package org.nrnb.pathexplorer;
 
+import java.util.Properties;
+
 import org.cytoscape.app.swing.AbstractCySwingApp;
 import org.cytoscape.app.swing.CySwingAppAdapter;
+import org.cytoscape.application.swing.CyNodeViewContextMenuFactory;
+import org.cytoscape.service.util.CyServiceRegistrar;
+import org.nrnb.pathexplorer.ui.AddAsSource;
 
 public class PathExplorer extends AbstractCySwingApp {
 	public PathExplorer(CySwingAppAdapter adapter)
 	{
 		super(adapter);
+	  	final CyServiceRegistrar registrar = adapter.getCyServiceRegistrar();
+		registrar.registerService(new AddAsSource(),
+	                                  CyNodeViewContextMenuFactory.class,
+	                                  new Properties());
 	}
 
 }
