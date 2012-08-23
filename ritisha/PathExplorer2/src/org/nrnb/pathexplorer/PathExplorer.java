9a5b468ee8372db7621a4c0b4025ac38710bd631
isaacho <isaacho@23aac120-1128-46ae-97c9-9e165d6aae65>
2012-08-23 00:49:48 +0000

    major refactoring done

--- ritisha/PathExplorer2/src/org/nrnb/pathexplorer/PathExplorer.java
+++ ritisha/PathExplorer2/src/org/nrnb/pathexplorer/PathExplorer.java
@@ -6,13 +6,9 @@ import java.util.Set;
 
 import org.cytoscape.app.swing.AbstractCySwingApp;
 import org.cytoscape.app.swing.CySwingAppAdapter;
-import org.cytoscape.application.swing.CyNodeViewContextMenuFactory;
-import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNetworkManager;
 import org.cytoscape.model.CyNetworkTableManager;
-import org.cytoscape.model.CyNode;
-import org.cytoscape.model.CyTable;
 import org.cytoscape.model.CyTableFactory;
 import org.cytoscape.model.events.NetworkAddedListener;
 import org.cytoscape.service.util.CyServiceRegistrar;
@@ -21,15 +17,15 @@ import org.cytoscape.task.NodeViewTaskFactory;
 import org.cytoscape.view.vizmap.VisualMappingManager;
 import org.cytoscape.view.vizmap.VisualStyle;
 import org.cytoscape.view.vizmap.VisualStyleFactory;
-import org.nrnb.pathexplorer.logic.MyNetAddedListener;
-import org.nrnb.pathexplorer.tasks.RefreshState;
-import org.nrnb.pathexplorer.tasks.RefreshStateFromNode;
-import org.nrnb.pathexplorer.tasks.SelectPaths;
-import org.nrnb.pathexplorer.tasks.SelectPathsFromNode;
-import org.nrnb.pathexplorer.ui.AddAsSource;
-import org.nrnb.pathexplorer.tasks.ExcludeNode;
-import org.nrnb.pathexplorer.ui.SetAsTarget;
-import org.nrnb.pathexplorer.view.MyNetViewTaskFactory;
+import org.nrnb.pathexplorer.logic.FindAllPaths;
+import org.nrnb.pathexplorer.logic.TableHandler;
+import org.nrnb.pathexplorer.tasks.ClearPathsNetworkViewTaskFactory;
+import org.nrnb.pathexplorer.tasks.ClearPathsNodeViewTaskFactory;
+import org.nrnb.pathexplorer.tasks.ExcludeNetworkViewTaskFactory;
+import org.nrnb.pathexplorer.tasks.ExcludeNodeViewTaskFactory;
+import org.nrnb.pathexplorer.tasks.FindPathsNodeViewTaskFactory;
+import org.nrnb.pathexplorer.tasks.SelectPathsNetworkViewTaskFactory;
+import org.nrnb.pathexplorer.tasks.SelectPathsNodeViewTaskFactory;
 
 public class PathExplorer extends AbstractCySwingApp {
 	
@@ -45,8 +41,6 @@ public class PathExplorer extends AbstractCySwingApp {
 		super(adapter);
 	  	final CyServiceRegistrar registrar = adapter.getCyServiceRegistrar();
 		Set<CyNetwork> allNets = new HashSet<CyNetwork>();
-	  	CyTable hiddenNodeTable;
-	  	CyTable hiddenEdgeTable;
 	  	VisualStyle currVisualStyle;
 	  	myVisMappingManager = adapter.getVisualMappingManager();
 	  	myVisStyleFactory = adapter.getVisualStyleFactory();
@@ -60,31 +54,75 @@ public class PathExplorer extends AbstractCySwingApp {
 	  	System.out.println("Got all networks, adding IF");
 	  	for(CyNetwork currNet : allNets)
 	  	{
-	  		hiddenNodeTable = currNet.getTable(CyNode.class, CyNetwork.HIDDEN_ATTRS);
-	  		hiddenNodeTable.createColumn("isExcludedFromPaths", Boolean.class, true, Boolean.FALSE);
-	  		hiddenNodeTable.createColumn("isInPath", Boolean.class, true, Boolean.FALSE);
-	  		hiddenEdgeTable = currNet.getTable(CyEdge.class, CyNetwork.HIDDEN_ATTRS);
-	  		hiddenEdgeTable.createColumn("isInPath", Boolean.class, true, Boolean.FALSE);
-	  		
+	  		//initialize hidden tables for each network
+	  		TableHandler.createColumns(currNet);
+
 	  	}
 	  	
 	  	//for newly added networks
-	  	registrar.registerService(new MyNetAddedListener(), NetworkAddedListener.class, new Properties());
-	  	
-	  	//registering all the context menus:-
-	  	
-	  	//Find paths from here (Add as Source) in Node context menu
-	  	registrar.registerService(new AddAsSource(adapter),
-	                                  CyNodeViewContextMenuFactory.class,
-	                                  new Properties());
-	  	
-	  	//Find paths to here (Set as Target) in Node context menu
-	  	registrar.registerService(new SetAsTarget(adapter),CyNodeViewContextMenuFactory.class,
-                new Properties());
+	  	registrar.registerService(new TableHandler(), NetworkAddedListener.class, new Properties());
 	  	
 	  	
+	  	/*
+	  	 * Registering NODE context menus
+	  	 */
 	  	
-	   //Exclude nodes with.. in context menu of network
+	  	//Find paths FROM here 
+	  	Properties findPathsFromProps = new Properties();
+	  	findPathsFromProps.setProperty("preferredAction", "NEW");
+	  	findPathsFromProps.setProperty("preferredMenu", "PathExplorer[100]");
+	  	findPathsFromProps.setProperty("menuGravity", "6.0f");
+	  	findPathsFromProps.setProperty("title", "Find paths from here");
+	  	
+	  	registrar.registerService(new FindPathsNodeViewTaskFactory(adapter, FindAllPaths.FROM_HERE), 
+	  			NodeViewTaskFactory.class, findPathsFromProps);
+	  	  	
+	  	//Find paths TO here 
+	  	Properties findPathsToProps = new Properties();
+	  	findPathsToProps.setProperty("preferredAction", "NEW");
+	  	findPathsToProps.setProperty("preferredMenu", "PathExplorer[100]");
+	  	findPathsToProps.setProperty("menuGravity", "7.0f");
+	  	findPathsToProps.setProperty("title", "Find paths to here");
+	  	
+	  	registrar.registerService(new FindPathsNodeViewTaskFactory(adapter, FindAllPaths.TO_HERE), 
+	  			NodeViewTaskFactory.class, findPathsToProps);
+	  	
+	  	//Exclude nodes 
+	  	Properties excludeNodeProps = new Properties();
+	  	excludeNodeProps.setProperty("preferredAction", "NEW");
+	  	excludeNodeProps.setProperty("preferredMenu", "PathExplorer[100]");
+	  	excludeNodeProps.setProperty("menuGravity", "8.0f");
+	  	excludeNodeProps.setProperty("title", "Exclude node");
+	  	
+	  	registrar.registerService(new ExcludeNodeViewTaskFactory(), 
+	  			NodeViewTaskFactory.class, excludeNodeProps);
+	  	
+	  	//Select Paths 
+	  	Properties selectPathsNodeProps = new Properties();
+	  	selectPathsNodeProps.setProperty("preferredAction", "NEW");
+	  	selectPathsNodeProps.setProperty("preferredMenu", "PathExplorer[100]");
+	  	selectPathsNodeProps.setProperty("menuGravity", "9.0f");
+	  	selectPathsNodeProps.setProperty("title", "Select all paths");
+	  	
+	  	registrar.registerService(new SelectPathsNodeViewTaskFactory(), 
+	  			NodeViewTaskFactory.class, selectPathsNodeProps);
+	  	
+	  	
+	  	//Clear Paths (Refresh button) 
+	  	Properties refreshNodeProps = new Properties();
+	  	refreshNodeProps.setProperty("preferredAction", "NEW");
+	  	refreshNodeProps.setProperty("preferredMenu", "PathExplorer[100]");
+	  	refreshNodeProps.setProperty("menuGravity", "10.0f");
+	  	refreshNodeProps.setProperty("title", "Clear all paths");
+	  	
+	  	registrar.registerService(new ClearPathsNodeViewTaskFactory(adapter), 
+	  			NodeViewTaskFactory.class, refreshNodeProps);
+	  	
+	  	/*
+	  	 * Registering NETWORK context menus
+	  	 */
+	  	
+	   //Exclude nodes with.. 
 	  	Properties excludeNodesWithProps = new Properties();
 	  	excludeNodesWithProps.setProperty("enableFor", "networkAndView");
 	  	excludeNodesWithProps.setProperty("preferredAction", "NEW");
@@ -92,61 +130,30 @@ public class PathExplorer extends AbstractCySwingApp {
 	  	excludeNodesWithProps.setProperty("menuGravity", "8.0f");
 	  	excludeNodesWithProps.setProperty("title", "Exclude Nodes With..");
 	  	
-	  	registrar.registerService(new MyNetViewTaskFactory(adapter), 
+	  	registrar.registerService(new ExcludeNetworkViewTaskFactory(adapter), 
 	  			NetworkViewTaskFactory.class, excludeNodesWithProps);
 	  	
-	  	//Exclude nodes in Node Context Menu
-	  	Properties excludeNodeProps = new Properties();
-	  	excludeNodeProps.setProperty("preferredAction", "NEW");
-	  	excludeNodeProps.setProperty("preferredMenu", "PathExplorer[100]");
-	  	excludeNodeProps.setProperty("menuGravity", "8.0f");
-	  	excludeNodeProps.setProperty("title", "Exclude node");
-	  	
-	  	registrar.registerService(new ExcludeNode(), 
-	  			NodeViewTaskFactory.class, excludeNodeProps);
-	  	
-	  	
-	  	//Select Paths in Network Context Menu
+	  	//Select Paths 
 	  	Properties selectPathsProps = new Properties();
 	  	selectPathsProps.setProperty("enableFor", "networkAndView");
 	  	selectPathsProps.setProperty("preferredAction", "NEW");
 	  	selectPathsProps.setProperty("preferredMenu", "PathExplorer[100]");
 	  	selectPathsProps.setProperty("menuGravity", "9.0f");
-	  	selectPathsProps.setProperty("title", "Select Paths");
+	  	selectPathsProps.setProperty("title", "Select all paths");
 	  	
-	  	registrar.registerService(new SelectPaths(), 
+	  	registrar.registerService(new SelectPathsNetworkViewTaskFactory(), 
 	  			NetworkViewTaskFactory.class, selectPathsProps);
 	  	
-	  	//Select Paths in Node Context Menu
-	  	Properties selectPathsNodeProps = new Properties();
-	  	selectPathsNodeProps.setProperty("preferredAction", "NEW");
-	  	selectPathsNodeProps.setProperty("preferredMenu", "PathExplorer[100]");
-	  	selectPathsNodeProps.setProperty("menuGravity", "9.0f");
-	  	selectPathsNodeProps.setProperty("title", "Select Paths");
-	  	
-	  	registrar.registerService(new SelectPathsFromNode(), 
-	  			NodeViewTaskFactory.class, selectPathsNodeProps);
-	  	
-	  	//Clear Paths (Refresh button) in Network Context Menu
+	  	//Clear Paths (Refresh button) 
 	  	Properties refreshProps = new Properties();
 	  	refreshProps.setProperty("enableFor", "networkAndView");
 	  	refreshProps.setProperty("preferredAction", "NEW");
 	  	refreshProps.setProperty("preferredMenu", "PathExplorer[100]");
 	  	refreshProps.setProperty("menuGravity", "10.0f");
-	  	refreshProps.setProperty("title", "Clear Paths");
+	  	refreshProps.setProperty("title", "Clear all paths");
 	  	
-	  	registrar.registerService(new RefreshState(adapter), 
+	  	registrar.registerService(new ClearPathsNetworkViewTaskFactory(adapter), 
 	  			NetworkViewTaskFactory.class, refreshProps);
 	  	
-	  	
-	  	//Clear Paths (Refresh button) in Node Context Menu
-	  	Properties refreshNodeProps = new Properties();
-	  	refreshNodeProps.setProperty("preferredAction", "NEW");
-	  	refreshNodeProps.setProperty("preferredMenu", "PathExplorer[100]");
-	  	refreshNodeProps.setProperty("menuGravity", "10.0f");
-	  	refreshNodeProps.setProperty("title", "Clear Paths");
-	  	
-	  	registrar.registerService(new RefreshStateFromNode(adapter), 
-	  			NodeViewTaskFactory.class, refreshNodeProps);
 	}
 }
